616dadae712d08fd2d4e36eba46136bd5bb3bec0
Kevin Tenolli <tenollikevin@gmail.com>
2025-06-12 23:33:14 +0200

    fix: clean up code

--- staf/staf_extensions.cpp
+++ staf/staf_extensions.cpp
@@ -24,44 +24,24 @@ init_staf_(const torch::Tensor &col_ptr, const torch::Tensor &row_idx,
   int32_t *col_pointers = col_ptr.data_ptr<int32_t>();
   int32_t *row_indices = row_idx.data_ptr<int32_t>();
   float *array_of_values = values.data_ptr<float>();
-  // Example static input
-  /* int32_t row_indices[] = { */
-  /*     0, 2, 3, 4, // col 0 */
-  /*     0, 2, 3, 4, // col 1 */
-  /*     1,          // col 2 */
-  /*     1, 4,       // col 3 */
-  /*     0, 2, 3, 4, // col 4 */
-  /*     0, 2, 3, 4  // col 5 */
-  /* }; */
-  /* int32_t col_pointers[] = {0, 4, 8, 9, 11, 15, 19}; */
-  /* int32_t col_size = 6; */
 
   suffix_forest forest(nr_tries, score_lambda);
   forest.create_forest(col_pointers, row_indices, n_cols);
   auto binary_csr = forest.build_csr(n_rows);
 
-  auto row_ptr_tensor = torch::tensor(binary_csr.get_row_ptr(), torch::kInt32);
-  auto col_indices_tensor =
-      torch::tensor(binary_csr.get_col_indices(), torch::kInt32);
-  auto data_tensor = torch::tensor(binary_csr.get_data(), torch::kFloat32);
-  std::vector<torch::Tensor> csr_tensors = {row_ptr_tensor, col_indices_tensor,
-                                            data_tensor};
+  std::vector<torch::Tensor> csr_tensors = {
+      torch::tensor(binary_csr.get_row_ptr(), torch::kInt32),
+      torch::tensor(binary_csr.get_col_indices(), torch::kInt32),
+      torch::tensor(binary_csr.get_data(), torch::kFloat32)};
 
-  auto &[map_suffix_ptr, map_row_index] = binary_csr.get_mapped_rows();
-  auto map_suffix_ptr_tensor = torch::tensor(map_suffix_ptr, torch::kInt32);
-  auto map_row_index_tensor = torch::tensor(map_row_index, torch::kInt32);
-  std::vector<torch::Tensor> map_tensors = {map_suffix_ptr_tensor,
-                                            map_row_index_tensor};
+  std::vector<torch::Tensor> map_tensors = {
+      torch::tensor(std::get<0>(binary_csr.get_mapped_rows()), torch::kInt32),
+      torch::tensor(std::get<1>(binary_csr.get_mapped_rows()), torch::kInt32)};
 
-  const auto &suffix_row_ptr = binary_csr.get_suffix_row_ptr();
-  const auto &suffix_col_indices = binary_csr.get_suffix_col_indices();
-  const auto &suffix_data = binary_csr.get_suffix_data();
-
-  auto suffix_row_tensor = torch::tensor(suffix_row_ptr, torch::kInt32);
-  auto suffix_col_tensor = torch::tensor(suffix_col_indices, torch::kInt32);
-  auto suffix_data_tensor = torch::tensor(suffix_data, torch::kFloat32);
-  auto packed_suffix_data = {suffix_row_tensor, suffix_col_tensor,
-                             suffix_data_tensor};
+  std::vector<torch::Tensor> packed_suffix_data = {
+      torch::tensor(binary_csr.get_suffix_row_ptr(), torch::kInt32),
+      torch::tensor(binary_csr.get_suffix_col_indices(), torch::kInt32),
+      torch::tensor(binary_csr.get_suffix_data(), torch::kFloat32)};
 
   return std::make_tuple(csr_tensors, packed_suffix_data, map_tensors);
 }
